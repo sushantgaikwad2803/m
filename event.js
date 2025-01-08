78d9f42706f8377724dd3e55d9a546cbae1a0a46
sushantgaikwad2803 <sushantgaikwad2803@gmail.com>
2025-01-08 19:44:05 +0530

    t

--- event.js
+++ event.js
@@ -1,10 +1,8 @@
 
-
-
 window.addEventListener('scroll',function(){
   const header = document.querySelector("header"); 
 
-  header.classList.toggle("sticky",window.scrollY > 400);
+  header.classList.toggle("sticky",window.scrollY > 130);
 })
 
 
